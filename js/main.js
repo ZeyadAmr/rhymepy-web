e2d01a4c5d4b327ac4dd421ac888ffe8f628cdda
zeyadamr <zeyadmeniawy@gmail.com>
2020-07-12 20:16:54 +0200

    Add box-shadow to input and output boxes for visibility, add placeholder for input textarea, add function to change textarea height on input

--- js/main.js
+++ js/main.js
@@ -8,22 +8,26 @@ const spinner = document.querySelector('.spinner')
 const rectangles = spinner.querySelectorAll("div") 
 const btns = document.querySelectorAll('.btn')
 const randomColor = colors[Math.floor(Math.random()*colors.length)]
+const textarea = document.querySelector('#text')
 let output = document.querySelector('.output')
 
 
 // Set a random color from the colors list as the logo's background color
 logoText.style.background = "linear-gradient(to top," + randomColor +" 30%, transparent 30%)"
-
 // Set a random color from the colors list as loading spinner's color
 rectangles.forEach(rect => rect.style.backgroundColor = randomColor)
-
 // Set a random color from the colors list as .btn background color
 btns.forEach(btn => btn.style.backgroundColor = randomColor)
-
+// Change textarea's height to match content height
+// textarea.style.maxHeight = 650 + "px"
+textarea.addEventListener("input", () => {
+    textarea.style.height = ""
+    textarea.style.height = textarea.scrollHeight + "px"
+})
 
 // Start Rhymepy
 analyzeButton.addEventListener("click", () => (async function() {
-    const textValue = document.querySelector('#text').value
+    const textValue = textarea.value
     const rhymingOption = document.querySelector('input[name="rhyming-options"]:checked').value 
     const rhymingMethod = document.querySelector('input[name="rhyming-methods"]:checked').value
     const lines = document.querySelector('input[name="lines-value"]').value
